b613505fa577050b3443e921c31b50a70ce43c05
Leah Tu <leahtu@microsoft.com>
2026-06-17 09:47:11 -0700

    Update garden title to Leah's Digital Garden

--- quartz/quartz.config.ts
+++ quartz/quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Leah's Garden",
+    pageTitle: "Leah's Digital Garden",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
